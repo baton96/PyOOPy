e8feb524640cab886904b9796c80d9acb3b73597
baton96 <podolce@op.pl>
2021-09-16 20:35:10 +0200

    Prepared packet for PyPI

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+    name='PyOOP',
+    packages=['PyOOP'],
+    version='0.1',
+    license='MIT',
+    description='Object-oriented programming in Python',
+    author='Bartosz Paulewicz',
+    author_email='podolce0@gmail.com',
+    url='https://gitlab.com/baton96/PyOOP',
+    download_url='https://gitlab.com/baton96/PyOOP/archive/v_01.tar.gz',
+    keywords=['OOP', 'Object-oriented programming'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ]
+)
