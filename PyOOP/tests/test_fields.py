60625e7b816010fad8083689e4509ca4e443f36e
baton96 <podolce@op.pl>
2021-09-16 18:35:47 +0200

    Structured packet

--- PyOOP/tests/test_fields.py
+++ PyOOP/tests/test_fields.py
@@ -1,6 +1,34 @@
-from models_fields import base, child
+from dataclasses import dataclass
 from PyOOP import PyOOP
 import pytest
+
+
+@dataclass
+class Base(PyOOP):
+    protected_field: PyOOP.Protected = 'success'
+    private_field: PyOOP.Private = 'success'
+    public_field: PyOOP.Public = 'success'
+
+    def protected_field_getter(self):
+        return self.protected_field
+
+    def private_field_getter(self):
+        return self.private_field
+
+    def public_field_getter(self):
+        return self.public_field
+
+
+class Intermediate(Base):
+    pass
+
+
+class Child(Intermediate):
+    pass
+
+
+child = Child()
+base = Base()
 
 
 # Base
--- PyOOP/tests/test_methods.py
+++ PyOOP/tests/test_methods.py
@@ -1,6 +1,42 @@
-from models_methods import base, child
+from dataclasses import dataclass
 from PyOOP import PyOOP
 import pytest
+
+
+@dataclass
+class Base(PyOOP):
+    @PyOOP.protected
+    def protected_method(self):
+        return 'success'
+
+    @PyOOP.private
+    def private_method(self):
+        return 'success'
+
+    @PyOOP.public
+    def public_method(self):
+        return 'success'
+
+    def protected_method_getter(self):
+        return self.protected_method()
+
+    def private_method_getter(self):
+        return self.private_method()
+
+    def public_method_getter(self):
+        return self.public_method()
+
+
+class Intermediate(Base):
+    pass
+
+
+class Child(Intermediate):
+    pass
+
+
+child = Child()
+base = Base()
 
 
 # Base
